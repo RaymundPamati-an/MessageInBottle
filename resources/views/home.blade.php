090ed8f36cf70ff6b5143812cf5d321f76737ccd
Raymund Pamati-an <rlpamatian@up.edu.ph>
2019-05-09 13:23:18 +0800

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Welcome to Message in a Bottle</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                        <a href="#" id="button" class="button">Create Message</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
